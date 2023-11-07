5f4a072e94ebbe16ccb9b4820858a4e113b53a8c
Mustafa Malviwala <102210999+Redheadedphantom@users.noreply.github.com>
2023-11-07 10:03:21 +0000

    commit

--- examples/demo.js
+++ examples/demo.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+console.log("Hello");
+
+var a = 10;
+var b = 20;
+
+function add(a, b) {
+  var c = a + b;
+  return c; // You need to return the result to use it later
+}
+
+console.log(add(a, b)); // Call the function with 'a' and 'b' as arguments
