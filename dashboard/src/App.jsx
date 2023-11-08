721c0847d55a07ad56ac868b4c4a2ab4b53a0f0b
Mustafa Malviwala <102210999+Redheadedphantom@users.noreply.github.com>
2023-11-08 12:22:28 +0000

    commit

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,34 +1,34 @@
-// import { useState } from 'react';
-import React from 'react';
-import './App.css';
-// import Login from './components/login';
-// import Logout from './components/Logout';
-// import { useEffect } from 'react';
-// import { gapi } from 'gapi-script';
-// import reactLogo from './assets/icon.jpg';
+// // import { useState } from 'react';
+// import React from 'react';
+// import './App.css';
+// // import Login from './components/login';
+// // import Logout from './components/Logout';
+// // import { useEffect } from 'react';
+// // import { gapi } from 'gapi-script';
+// // import reactLogo from './assets/icon.jpg';
 
-// const clientId = "905869336236-6ieq8pnle4qr730nug1i8um1qj3qh8vu.apps.googleusercontent.com";
+// // const clientId = "905869336236-6ieq8pnle4qr730nug1i8um1qj3qh8vu.apps.googleusercontent.com";
 
-function App() {
-//   useEffect(() => {
-//     function start() {
-//       gapi.client.init({
-//         clientId: clientId,
-//         scope: ""
-//       });
-//     }
+// function App() {
+// //   useEffect(() => {
+// //     function start() {
+// //       gapi.client.init({
+// //         clientId: clientId,
+// //         scope: ""
+// //       });
+// //     }
 
-//     gapi.load('client:auth2', start);
-//   }, []);
+// //     gapi.load('client:auth2', start);
+// //   }, []);
 
-  return (
-    <div>
-      <h1> Hello </h1>
-      {/* <img className="img-logo" src={reactLogo} alt="React Logo" /> */}
-      {/* <Login />
-      <Logout /> */}
-    </div>
-  );
+//   return (
+//     <div>
+//       <h1> Hello </h1>
+//       {/* <img className="img-logo" src={reactLogo} alt="React Logo" /> */}
+//       {/* <Login />
+//       <Logout /> */}
+//     </div>
+//   );
  
     }
 export default App;
